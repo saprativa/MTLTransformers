0165077e4e479aaefcb1921abedaa26547545233
Saprativa Bhattacharjee <saprativa@gmail.com>
2022-04-19 20:20:51 +0530

    num_beams

--- code/run_summarization.py
+++ code/run_summarization.py
@@ -218,7 +218,7 @@ class DataTrainingArguments:
         },
     )
     num_beams: Optional[int] = field(
-        default=None,
+        default=10,
         metadata={
             "help": "Number of beams to use for evaluation. This argument will be passed to ``model.generate``, "
             "which is used during ``evaluate`` and ``predict``."
